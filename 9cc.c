95a7c7f96c92e6fc12a3f2bad86ab76b62fe9c55
daicmod <daicmod@gmail.com>
2022-07-26 00:20:35 +0900

    単項演算子の追加

--- 9cc.c
+++ 9cc.c
@@ -157,6 +157,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -172,6 +173,14 @@ Node *expr() {
   }
 }
 
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), primary());
+  return primary();
+}
+
 Node *primary() {
   // 次のトークンが"("なら、"(" expr ")"のはず
   if (consume('(')) {
@@ -183,12 +192,12 @@ Node *primary() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
